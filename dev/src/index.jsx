6363311008562be8fca58e0b5c08521dff5ca879
Patrick Lake <plake492@gmail.com>
2023-04-16 19:27:22 -0400

    Add npmignore | add some addtional sass options

--- dev/src/index.jsx
+++ dev/src/index.jsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-
-// import '../node_modules/@plake492/t_css_utils/src/index.scss'
-// import '../node_modules/@plake492/t_css_utils/dist/reset.css'
-// import '../node_modules/@plake492/t_css_utils/dist/index.css'
 import './styles/app.scss'
 
 const rootElement = document.getElementById('root')
